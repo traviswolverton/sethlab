a65ecf5987e501de6e3573d16001fa419d5ac56a
traviswolverton <traviswolverton@gmail.com>
2026-03-23 07:39:53 -0500

    Added choice programming

--- global_thermonuclear_war/WOPR.py
+++ global_thermonuclear_war/WOPR.py
@@ -5,9 +5,29 @@ input_password = input ("Enter Password: ")
 is_logged_in = (input_username == stored_username) and (input_password == stored_password)
 if is_logged_in:
     print("Login Successful!")
-    print("Here is a list of games:")
-    my_list = ["1. Tic Tac Toe", "2. Quiz," "3. Chess"]
+    my_list = ["1. Tic Tac Toe",  "2. Quiz",  "3. Chess"]
+    print("Here is a list of games.")
     for item in my_list:
         print(item)
+    choice = ("Please select a game number: ")
+
+    if choice == 1:
+        print("Selected 1")
+        print("Starting Tic Tac Toe...")
+    #Tic-Tac-Toe programming HERE
+
+    elif choice == 2:
+        print("Selected 2")
+        print("Starting Quiz...")
+    #Quiz programming HERE
+
+    elif choice == 3:
+        print ("Selected 3")
+        print ("Starting Chess...")
+
+    else:
+        print("Invalid selection. Select a valid number.")
+
+
 else: 
     print("Login Failed. Invalid username/password.")
