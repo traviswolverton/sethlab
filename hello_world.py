86c95878abcc6f4350fd6a5504d6e8f264a50267
traviswolverton <traviswolverton@gmail.com>
2026-03-22 20:10:33 -0500

    Organized the code and fixed the grammar.

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,13 @@
 # hello_world.py
+#age factor authentication
 age = int(input("Please enter your age: "))
+is_of_voting_age = age >= 18
+#citizen factor authentication
 citizen_input = input("Are you a citizen? (y/n)" )
 is_citizen = citizen_input == "y"
-is_of_voting_age = age >= 18
+#legible or illegible to vote
 is_eligible_to_vote = is_of_voting_age and is_citizen
 if is_eligible_to_vote:
-    print("Contratulations! You are eligible to vote.")
+    print("Congratulations! You are eligible to vote.")
 else:
     print("You are not eligible to vote.")
