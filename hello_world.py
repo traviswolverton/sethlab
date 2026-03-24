6163e4f65ddcb99157577de777eb8d424cbff15e
traviswolverton <traviswolverton@gmail.com>
2026-03-24 07:29:45 -0500

    Made basic view profile option to dashboard

--- hello_world.py
+++ hello_world.py
@@ -5,13 +5,33 @@ stored_username = "sethwolverton514"
 
 def user_dashboard(username):
     while True:
-        print("Welcome to your dashboard, " + username + "!")
-        print("1. View Profile")
-        print("2. Manage Tasks")
+        print("Welcome," + input_username + "!")
+        print("1. Profile Management")
+        print("2. Manage Tasks")        
         print("3. Access Resources")
         print("4. Logout")
 
-        choice = input("Select an option: ")
+        choice == input("Please select an option: ")
+
+        if choice == '1':
+            view_profile(username)
+        elif choice == '2':
+            manage_tasks()
+        elif chouce == '3':
+            access_resources()
+        elif choice == '4':
+            print("Logging out...")
+            break
+        
+        else:
+            print("Invalid option. Please selct a valid choice.")
+
+def view_profile(username):
+    print("Profile Mangemnet for {username}")
+    print("Profile Name: " + {username})
+    #more option HERE
+    input("Press Enter to return to the dashboard")
+
 #input for both username and password
 input_username = input("Enter your username: ")
 input_password = input("Enter your password: ")
